7c1e2edc7bfbf203edac24d3b33c174413d31224
Massim Shah <massimshah@hotmail.com>
2021-06-27 00:04:26 +0900

    added gem

--- app/controllers/kanjis_controller.rb
+++ app/controllers/kanjis_controller.rb
@@ -1,2 +1,8 @@
 class KanjisController < ApplicationController
+  require 'rest-client'
+  def get_kanji
+    url = "https://kanjiapi.dev/v1/kanji/all"
+    rest_response = RestClient.get(url)
+    response = JSON.parse(rest_response.body)
+  end
 end
