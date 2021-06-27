78e4382bdd6f35d69f4d48d93cb4aee38d3cfa42
Massim Shah <massimshah@hotmail.com>
2021-06-27 19:19:09 +0900

    added ability to search by kanji readings

--- app/controllers/kanjis_controller.rb
+++ app/controllers/kanjis_controller.rb
@@ -1,7 +1,11 @@
 class KanjisController < ApplicationController
   def index
-    # rubocop:disable
-    @results = Kanji.retrieve_kanji("#{params[:query]}")
-    # rubocop:enable
+    if params[:dropdown_selection] == 'Kanji'
+      # rubocop:disable
+      @results = Kanji.retrieve_kanji("#{params[:query]}")
+      # rubocop:enable
+    else
+      @results = Kanji.retrieve_readings("#{params[:query]}")
+    end
   end
 end
--- app/models/kanji.rb
+++ app/models/kanji.rb
@@ -13,4 +13,13 @@ class Kanji < ApplicationRecord
       return 'No results found'
     end
   end
+
+  def self.retrieve_readings(param)
+    @url = "https://kanjiapi.dev/v1/reading/#{URI.encode(param.force_encoding('ASCII-8BIT'))}"
+    begin
+      JSON.parse(Kanji.get_data) if Kanji.get_data.code == 200
+    rescue RestClient::NotFound
+      return 'No results found'
+    end
+  end
 end
