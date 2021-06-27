b1dc866f9c27a9b918c1379cb649ff947b7040b5
Massim Shah <massimshah@hotmail.com>
2021-06-27 15:27:08 +0900

    added ability to call the kanji and display it

--- app/controllers/kanjis_controller.rb
+++ app/controllers/kanjis_controller.rb
@@ -1,8 +1,5 @@
 class KanjisController < ApplicationController
-  require 'rest-client'
-  def get_kanji
-    url = "https://kanjiapi.dev/v1/kanji/all"
-    rest_response = RestClient.get(url)
-    response = JSON.parse(rest_response.body)
+  def index
+    @results = Kanji.retrieve_results("#{params[:query]}")
   end
 end
--- app/models/kanji.rb
+++ app/models/kanji.rb
@@ -1,2 +1,14 @@
 class Kanji < ApplicationRecord
+  require 'rest-client'
+  require 'uri'
+  def self.get_data
+    RestClient.get(@url, { :content_type => :json })
+  end
+
+  def self.retrieve_results(param)
+    unless param.empty?
+      @url = "https://kanjiapi.dev/v1/kanji/#{URI::encode(param.force_encoding('ASCII-8BIT'))}"
+      JSON.parse(Kanji.get_data)
+    end
+  end
 end
