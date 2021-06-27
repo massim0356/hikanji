3ac545357546da98606319a1264a7fbb94a79741
Massim Shah <massimshah@hotmail.com>
2021-06-27 16:31:19 +0900

    created begin and rescues for 404 errors

--- app/controllers/kanjis_controller.rb
+++ app/controllers/kanjis_controller.rb
@@ -1,5 +1,6 @@
 class KanjisController < ApplicationController
   def index
-    @results = Kanji.retrieve_results("#{params[:query]}")
+    @results = Kanji.retrieve_results("#{params[:query]}");
+    raise
   end
 end
--- app/models/kanji.rb
+++ app/models/kanji.rb
@@ -2,13 +2,15 @@ class Kanji < ApplicationRecord
   require 'rest-client'
   require 'uri'
   def self.get_data
-    RestClient.get(@url, { :content_type => :json })
+    RestClient.get(@url, { content_type: :json })
   end
 
   def self.retrieve_results(param)
-    unless param.empty?
       @url = "https://kanjiapi.dev/v1/kanji/#{URI::encode(param.force_encoding('ASCII-8BIT'))}"
-      JSON.parse(Kanji.get_data)
-    end
+      begin
+        JSON.parse(Kanji.get_data) if Kanji.get_data.code == 200
+      rescue RestClient::NotFound
+        return 'No results found'
+      end
   end
 end
