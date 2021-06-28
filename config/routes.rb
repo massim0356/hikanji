4a173b7a53584631e0ab0cbc6ed02a14b605bb08
Massim Shah <massimshah@hotmail.com>
2021-06-28 15:29:00 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :kanjis
-  root to: 'pages#home'
+  resources :kanjis, only:[:index]
+  root to: 'kanjis#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
